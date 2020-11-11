d1a1add5a4773bfb0eca51f8d58518965cb5b1f0
enzobo <enzobo@github.com>
2020-11-10 21:00:45 -0300

    .

--- Assessoria 05 - Adriana/Rotinas/visualização.R
+++ Assessoria 05 - Adriana/Rotinas/visualização.R
@@ -8,7 +8,7 @@ require(webr)
 
 # Análise Descritiva ------------------------------------------------------
 
-dfSummary(df)
+dfSummary(df) # apenas para nossa visualização
 
 
 # Visualização Gráfica ----------------------------------------------------
@@ -26,52 +26,112 @@ ggplot(df,aes(x = Raiva, fill = Municipio))+
 # número de casos por sexo
 
 ggplot(df, aes(x = Sexo, fill = Raiva))+
-  geom_bar(position=position_dodge())+
+  geom_bar(position=position_dodge(), color = 'black')+
+  theme_minimal()+
   geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
             position = position_dodge(width = 0.9))+
-  lims(y = c(0,30))
+  lims(y = c(0,30))+
+  labs(y = 'Contagem')+
+  scale_fill_discrete(labels = c('Ausente','Presente'))
 
 # número de casos por idade
 
 ggplot(df, aes(x = Idade, fill = Raiva))+
-  geom_bar(position=position_dodge())+
+  geom_bar(position=position_dodge(), colour = 'black')+
+  theme_minimal()+
   geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
             position = position_dodge(width = 0.9))+
-  scale_x_continuous(n.breaks = 40)
-  
+  labs(y = 'Contagem')+
+  lims(y = c(0,15))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))
 
-tb = function(x){
-  as.data.frame(table(x, useNA = 'ifany')/length(x)) 
-}
+# número de casos por origem
 
-# pie chart
-
-
-ggplot(tst$Sexo, aes(x = '', y = Freq, fill = x))+
-  geom_bar(width = 1, stat = 'identity')+
-  coord_polar("y", start = 0)+
+ggplot(df, aes(x = Origem, fill = Raiva))+
+  geom_bar(position=position_dodge(), colour = 'black')+
   theme_minimal()+
-  theme(
-    panel.border = element_blank(),
-    panel.grid = element_blank(),
-    axis.text.x = element_blank())+
-  geom_text(aes(label = paste(round(Freq / sum(Freq) * 100, 1), "%")),
-            position = position_stack(vjust = 0.5))+
-  labs(x = 'teste')
+  geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
+            position = position_dodge(width = 0.9))+
+  labs(y = 'Contagem')+
+  lims(y = c(0,40))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))
+
+# número de casos por Sinais
+
+ggplot(df, aes(x = SinaisA, fill = Raiva))+
+  geom_bar(position=position_dodge(), colour = 'black')+
+  theme_minimal()+
+  geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
+            position = position_dodge(width = 0.9))+
+  labs(y = 'Contagem')+
+  lims(y = c(0,30))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))+
+  scale_x_discrete(labels = c('Ausente','Presente','NA'))
+
+# ...
+ggplot(df, aes(x = SinaisABC, fill = Raiva))+
+  geom_bar(position=position_dodge(), colour = 'black')+
+  theme_minimal()+
+  geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
+            position = position_dodge(width = 0.9))+
+  labs(y = 'Contagem')+
+  lims(y = c(0,40))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))+
+  scale_x_discrete(labels = c('Ausente','Presente','NA'))
+
+#...
+ggplot(df, aes(x = SinaisAC, fill = Raiva))+
+  geom_bar(position=position_dodge(), colour = 'black')+
+  theme_minimal()+
+  geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
+            position = position_dodge(width = 0.9))+
+  labs(y = 'Contagem')+
+  lims(y = c(0,40))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))+
+  scale_x_discrete(labels = c('Ausente','Presente','NA'))
+
+#...
+ggplot(df, aes(x = SinaisB, fill = Raiva))+
+  geom_bar(position=position_dodge(), colour = 'black')+
+  theme_minimal()+
+  geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
+            position = position_dodge(width = 0.9))+
+  labs(y = 'Contagem')+
+  lims(y = c(0,30))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))+
+  scale_x_discrete(labels = c('Ausente','Presente','NA'))
+
+#...
+ggplot(df, aes(x = SinaisBC, fill = Raiva))+
+  geom_bar(position=position_dodge(), colour = 'black')+
+  theme_minimal()+
+  geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
+            position = position_dodge(width = 0.9))+
+  labs(y = 'Contagem')+
+  lims(y = c(0,40))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))+
+  scale_x_discrete(labels = c('Ausente','Presente','NA'))
+
+#...
+ggplot(df, aes(x = SinaisC, fill = Raiva))+
+  geom_bar(position=position_dodge(), colour = 'black')+
+  theme_minimal()+
+  geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
+            position = position_dodge(width = 0.9))+
+  labs(y = 'Contagem')+
+  lims(y = c(0,40))+
+  scale_fill_discrete(labels = c('Ausente','Presente'))+
+  scale_x_discrete(labels = c('Ausente','Presente','NA'))
 
 
-g = compareGroups(Raiva ~ . - `Data da coleta`, data = df, method = c(Idade = NA))
+# Incidência por Mês/Semana
 
-pt = function(v1,v2){
-  teste = data.frame(select(df,all_of(v1)),
-                     select(df,all_of(v2)))
-  colnames(teste)= c('x','y')
-  ggplot(teste, aes(x = x, fill = y))+
-    geom_bar(position=position_dodge())+
-    geom_text(aes(label=stat(count)),stat='count',vjust = - 0.3,
-              position = position_dodge(width = 0.9))+
-    labs(x = eval(v1), fill = eval(v2), y = 'Contagem')+
-    theme_minimal()
-}
+df[which(df$Raiva == 1),1] %>%
+  incidence(., interval = '30',first_date = '2018-01-01',last_date = '2018-12-31',standard = F) %>% 
+  plot(., color = 'black', border = 'white')
 
-df %>% arrange(`Data da coleta`) %>% mutate(Soma = cumsum(as.numeric(as.character(Raiva))))%>%ggplot(aes(x = `Data da coleta`, y = Soma, col = Raiva))+geom_point()+theme_minimal()+lims(y=c(0,40))
+df[which(df$Raiva == 1),1] %>%
+  incidence(., interval = 'week',first_date = '2018-01-01',last_date = '2018-12-31',standard = F) %>% 
+  plot(., color = 'black', border = 'white')
+
+
