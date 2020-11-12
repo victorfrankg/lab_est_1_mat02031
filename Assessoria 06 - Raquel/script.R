3a5475a4ca0f8e8b9b20de24ac396b294d247436
nmhahn <nicolasmahahn@gmail.com>
2020-11-12 16:57:03 -0300

    bla bla

--- Assessoria 06 - Raquel/script.R
+++ Assessoria 06 - Raquel/script.R
@@ -15,7 +15,22 @@ str(machos)
 
 machos$agua = as.numeric(machos$agua)
 
+# valores trocados (outliers digitados errados?):
+# * massa
+# - rato 9 do g2 valor 4772 para 477,2 
+# - rato 9 do cn valor 4559 para 455,9
+# * alimento
+# - rato 4 do cn valor 2709 para 27,9
+# - rato 9 do g2 valor 293,7 para 29,7
+# * agua
+# - rato 5 do cn valor 335 para 35
+
 summary(machos$agua)
+
+machos$periodo = relevel(machos$periodo,"acasalamento") 
+machos$periodo = relevel(machos$periodo,"pre acasalamento") 
+machos$periodo = relevel(machos$periodo,"adaptativo") 
+
 
 library(ggplot2)
 ggplot(data=machos, aes(x=t, y=massa, group=interaction(t, grupo))) +
